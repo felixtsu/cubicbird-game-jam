93462955dcc60810c74d55b10a2c4779fb253db1
felixtsu <humaeks@gmail.com>
2020-08-19 21:35:44 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -22,10 +22,9 @@ namespace gamejam {
         return array;
     }
 
-    function drawIntro() {  
-        scene.backgroundImage().fill(13)        
-        scene.backgroundImage().printCenter('Cubicbird Game Jam', 50, 2, image.font8)
-        scene.backgroundImage().printCenter(' -- Room -- ', 70, 12, image.font8)
+    function drawBackground() {
+        scene.backgroundImage().fill(13)     
+        // scene.backgroundImage().fillCircle(80, 22, 20, 1)
         scene.backgroundImage().drawTransparentImage(img`
             . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -59,51 +58,39 @@ namespace gamejam {
             . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-        `, 64, 8)
+        `, 64, 8)  
+    }
+
+    function drawTriangle() {
+        scene.backgroundImage().drawLine(80 - image.font8.charWidth * 9, 50 + image.font8.charHeight, 80 + image.font8.charWidth * 9, 50 + image.font8.charHeight, 2)
+        for (let i = 80 - image.font8.charWidth * 9; i <= 80 + image.font8.charWidth * 9; i+=image.font8.charWidth * 2) {
+            scene.backgroundImage().drawLine(80, 50 + image.font8.charHeight + 10, i, 50 + image.font8.charHeight, 2)
+        }
+    }
+
+    function drawIntro() {  
+        drawBackground()
+        scene.backgroundImage().printCenter('Cubicbird Game Jam', 50, 2, image.font8)
+
+        drawTriangle()
+
+        scene.backgroundImage().fillRect(80 - image.font8.charWidth * 2 - 1, 69, image.font8.charWidth * 4 + 2, image.font8.charHeight + 2, 12)
+        scene.backgroundImage().fillRect(80 - image.font8.charWidth * 2, 70, image.font8.charWidth * 4, image.font8.charHeight, 1)
+
+        scene.backgroundImage().printCenter(' -- Room -- ', 70, 12, image.font8)
+        
+
         scene.backgroundImage().printCenter('Press Any Button', 90, 2, image.font8)
         game.waitAnyButton()
         scene.backgroundImage().fill(0)
     }
 
     export function drawNextRoom(author:string, roomTitle:string) {
-        scene.backgroundImage().fill(13)        
+        drawBackground()
         scene.backgroundImage().printCenter(author, 40, 2, image.font12)
         scene.backgroundImage().printCenter('presents', 60, 2, image.font8)
-        scene.backgroundImage().printCenter(roomTitle, 80, 12, image.font12)
-        scene.backgroundImage().drawTransparentImage(img`
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . . . . . .
-            . . . . . . 5 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . . . . . .
-            . . . . . 5 5 4 4 4 4 4 f f 4 4 4 4 4 4 4 4 4 4 4 . . . . . . .
-            . . . . 5 5 5 4 4 4 4 4 f f 4 4 4 4 4 9 9 9 9 9 9 9 9 9 9 9 9 .
-            . . . 5 5 5 5 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 9 9 9 9 9 . .
-            . . 5 5 5 5 5 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 9 9 9 9 . . .
-            . 5 5 5 5 5 5 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 9 9 9 . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 9 9 . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 9 . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 9 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 9 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 9 4 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 9 4 4 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 9 4 4 4 4 . . . . . . .
-            . . . . . . . 4 4 4 4 4 4 4 4 4 4 4 4 9 4 4 4 4 4 . . . . . . .
-            . . . . . . . 9 9 9 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . 9 9 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . . 9 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . . . 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . . . . 9 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . . . . . 9 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-        `, 64, 8)
+        scene.backgroundImage().drawLine(80 - image.font8.charWidth * 4, 60 + image.font8.charHeight, 80 + image.font8.charWidth * 4, 60 + image.font8.charHeight, 2)
+        scene.backgroundImage().printCenter(roomTitle, 80, 12, image.font8)
         scene.backgroundImage().printCenter('Press Any Button', 100, 2, image.font8)
         game.waitAnyButton()
         scene.backgroundImage().fill(0)
